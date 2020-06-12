d120f27d997c80944829a6f6293d310826d24035
RisKKon <tnmss@abv.bg>
2020-06-13 00:00:27 +0300

    v4

---
+++ v4/PartyHard.java
@@ -0,0 +1,14 @@
+public class PartyHard extends Tile {
+
+    public PartyHard(int row, int col) {
+
+        this.row = row;
+        this.col = col;
+    }
+
+
+    @Override
+    public void actionOnLanding(Player player) {
+
+    }
+}
---
+++ v4/Start.java
@@ -0,0 +1,15 @@
+public class Start extends Tile {
+
+    public Start(int row, int col) {
+
+        this.row = row;
+        this.col = col;
+
+    }
+
+
+    @Override
+    public void actionOnLanding(Player player) {
+
+    }
+}
